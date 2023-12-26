3524236baa6ce818d48c606896b02dfbffe0e840
Savio Soares <dsaviosnogueira@gmail.com>
2023-12-25 22:32:04 -0300

    Created splash screen sing

---
+++ app/src/main/java/com/example/personnotes/SplahScreenSing.kt
@@ -0,0 +1,11 @@
+package com.example.personnotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplahScreenSing : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splah_screen_sing)
+    }
+}
