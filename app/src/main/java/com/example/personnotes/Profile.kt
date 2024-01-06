7dd757fc2fb867a39f3cbc50e08a904e4004d8aa
Savio Soares <dsaviosnogueira@gmail.com>
2024-01-05 23:37:13 -0300

    new resources in screen profile

--- app/src/main/java/com/example/personnotes/Profile.kt
+++ app/src/main/java/com/example/personnotes/Profile.kt
@@ -1,25 +1,32 @@
 package com.example.personnotes
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Profile.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class Profile : Fragment() {
+
+    private lateinit var auth : FirebaseAuth
+    private lateinit var btnExit : TextView
+
+    private var emailUserScreen : TextView? = null
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +34,11 @@ class Profile : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+        auth = Firebase.auth
+
+
+
     }
 
     override fun onCreateView(
@@ -37,16 +49,25 @@ class Profile : Fragment() {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        emailUserScreen = view.findViewById(R.id.user_email)
+        emailUserScreen?.text = FirebaseAuth.getInstance().currentUser?.email
+
+
+        btnExit = view.findViewById(R.id.btn_exit)
+        btnExit.setOnClickListener {
+
+            FirebaseAuth.getInstance().signOut()
+
+            val intent = Intent(requireContext(), Login::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+        }
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Profile.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             Profile().apply {
